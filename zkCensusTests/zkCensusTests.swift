ae455a6bcecb5b799f6647c75b7d1197034cad4f
ayushshrivastv <ayush.srivastav@icloud.com>
2025-11-18 17:04:27 +0530

    Initial Commit

---
+++ zkCensusTests/zkCensusTests.swift
@@ -0,0 +1,17 @@
+//
+//  zkCensusTests.swift
+//  zkCensusTests
+//
+//  Created by AYUSH SRIVASTAVA on 18/11/25.
+//
+
+import Testing
+@testable import zkCensus
+
+struct zkCensusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
